4dc89b4056501faef6fc06aa2576623cab27cef1
ahmed karim <ahmed.karim.lahmar@gmail.com>
2023-01-20 15:44:43 +0100

    feat: update InfoToast for task finish

--- src/components/InfoToast.jsx
+++ src/components/InfoToast.jsx
@@ -1,28 +1,30 @@
 import React from "react";
 import { Toast, ToastContainer } from "react-bootstrap";
-import { PencilSquare, Trash } from "react-bootstrap-icons";
+import { CheckCircle, PencilSquare, Trash } from "react-bootstrap-icons";
 
 function InfoToast(props) {
   return (
-    <ToastContainer className="p-3" position={"top-end"}>
-      <Toast {...props} delay={3000} autohide>
-        <Toast.Header
-          className={
-            props.data.type === "delete" ? "text-danger" : "text-success"
-          }
-        >
-          {props.data.type === "delete" && <Trash />}
-          {props.data.type === "edit" && <PencilSquare />}
-          <strong className={`me-auto text-${props.bg}`}>
-            &nbsp;{props.data.title}
-          </strong>
-        </Toast.Header>
-        <Toast.Body className="text-white">
-          {props.data.msg} {props.data.title}.
-        </Toast.Body>
-      </Toast>
-    </ToastContainer>
+    <Toast {...props}>
+      <Toast.Header
+        className={
+          props.data.type === "delete" ? "text-danger" : "text-success"
+        }
+      >
+        {props.data.type === "delete" && <Trash />}
+        {props.data.type === "edit" && <PencilSquare />}
+        {props.data.type === "done" && <CheckCircle />}
+        <strong className={`me-auto text-${props.bg}`}>
+          &nbsp;{props.data.title}
+        </strong>
+      </Toast.Header>
+      <Toast.Body className="text-white">
+        {props.data.type === "done"
+          ? `${props.data.title} ${props.data.msg}`
+          : `${props.data.msg} ${props.data.title}.`}
+      </Toast.Body>
+    </Toast>
   );
 }
 
 export default InfoToast;
+  
