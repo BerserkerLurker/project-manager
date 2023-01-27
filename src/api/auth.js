e6f415ecb106ade6e4fd4f38fb8323ac2f83afe7
ahmed karim <ahmed.karim.lahmar@gmail.com>
2023-01-27 15:47:35 +0100

    feat: add updateuser and checkemail in auth api

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,5 +1,13 @@
 import axios from "axios";
-import { loginUri, logoutUri, signUpUri, url } from ".";
+import {
+  checkEmailUri,
+  loginUri,
+  logoutUri,
+  signUpUri,
+  updateUserUri,
+  url,
+} from ".";
+import jwtInterceptor from "./helpers/jwtInterceptor";
 
 export async function login(params) {
   try {
@@ -37,5 +45,32 @@ export async function logout() {
     globalThis.accessToken = "";
   } catch (error) {
     console.log(error);
+  }
+}
+
+export async function updateUser(params) {
+  try {
+    console.log(params);
+    const res = await jwtInterceptor.patch(url + updateUserUri, params, {
+      withCredentials: true,
+      headers: { Authorization: `Bearer ${globalThis.accessToken}` },
+    });
+    globalThis.accessToken = res.data.accessToken;
+    return res.data.user;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function checkEmail(email) {
+  try {
+    const res = await jwtInterceptor.post(url + checkEmailUri, email, {
+      withCredentials: true,
+      headers: { Authorization: `Bearer ${globalThis.accessToken}` },
+    });
+    // console.log(res.data);
+    return res.data;
+  } catch (error) {
+    throw(error);
   }
 }
